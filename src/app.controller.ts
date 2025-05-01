8bbd6b575ed0fcd1913cd12eb099800e7c754439
dominic-source <chinonsodomnic@gmail.com>
2025-05-01 17:20:05 +0100

    feat(app): add redirect feature

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiService } from './api/api.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+  ) {}
+
+  private readonly URL_404 = 'http://localhost:3000/404';
 
   @Get(':urlPath')
-  redirectUrl(@Param('urlPath') urlPath: string): string {
-    return this.appService.redirectUrl();
+  @Redirect()
+  redirectUrl(@Param('urlPath') urlPath: string) {
+    const longUrl = this.appService.getRedirectUrl(urlPath);
+    // if an error occurs, return a 404 page
+    if (!longUrl) {
+      return { url: this.URL_404 }; // Redirect to a 404 page
+    }
+    return { url: longUrl };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ApiService } from './api/api.service';
 
 @Injectable()
 export class AppService {
-  redirectUrl(): string {
-    return 'Hello World!';
+  constructor(private readonly apiService: ApiService) {}
+  
+  getRedirectUrl(urlPath: string): string | boolean {
+    // Get the long URL from the short URL
+    const shortUrl = this.apiService.prefixUrl(urlPath);
+    const longUrl = this.apiService.decodeUrl({ shortUrl });
+    if (longUrl === 'URL not found') {
+      return false; // URL not found
+    }
+    this.apiService.visitShortUrl(shortUrl)
+    return longUrl; // Redirect to the long URL
   }
 }
