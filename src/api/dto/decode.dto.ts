dd616cac0b9a47d48e00fa94446d6a0cc76aa2df
dominic-source <chinonsodomnic@gmail.com>
2025-05-01 17:16:25 +0100

    fix(api): eliminate top level domain bug in dto

--- src/api/dto/decode.dto.ts
+++ src/api/dto/decode.dto.ts
@@ -3,6 +3,6 @@ import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 export class DecodeDto {
   @IsNotEmpty({ message: 'Short URL cannot be empty' })
   @IsString({ message: 'Short URL must be a string' })
-  @IsUrl({}, { message: 'Must be a valid URL' })
+  @IsUrl({ require_tld: false }, { message: 'Must be a valid URL' })
   shortUrl: string;
 }
--- src/api/dto/encode.dto.ts
+++ src/api/dto/encode.dto.ts
@@ -3,6 +3,6 @@ import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 export class EncodeDto {
   @IsNotEmpty({ message: 'URL cannot be empty' })
   @IsString({ message: 'URL must be a string' })
-  @IsUrl({}, { message: 'Must be a valid URL' })
+  @IsUrl({ require_tld: false }, { message: 'Must be a valid URL' })
   longUrl: string;
 }
